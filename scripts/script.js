3b0ad7f895a0c48fa042eb858789acc865441efb
Balpreet Sekhon <balpreetsekhon787@gmail.com>
2024-03-06 12:30:29 -0800

    Fix missing ending tags

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,9 @@
 function info_handler() {
+
     jQuery('#info-box').slideToggle()
+}
+function redirect_to_login() {
+    window.location.href = 'login.html';
 }
 
 function homepage_handler() {
@@ -14,51 +18,45 @@ function homepage_handler() {
 
 function leaderboard_handler() {
     if (jQuery('#leaderboard').css("display") == "none") {
-    jQuery('#leaderboard').toggle();
-    jQuery('#homepage').css("display", "none");
-    jQuery('#activity_feed').css("display", "none");
-    jQuery('#datepicker').css("display", "none");
-    jQuery('#settings').css("display", "none");
+        jQuery('#leaderboard').toggle();
+        jQuery('#homepage').css("display", "none");
+        jQuery('#activity_feed').css("display", "none");
+        jQuery('#datepicker').css("display", "none");
+        jQuery('#settings').css("display", "none");
     }
 }
 
 function activity_handler() {
     if (jQuery('#activity_feed').css("display") == "none") {
-    jQuery('#activity_feed').toggle();
-    jQuery('#homepage').css("display", "none");
-    jQuery('#leaderboard').css("display", "none");
-    jQuery('#datepicker').css("display", "none");
-    jQuery('#settings').css("display", "none");
+        jQuery('#activity_feed').toggle();
+        jQuery('#homepage').css("display", "none");
+        jQuery('#leaderboard').css("display", "none");
+        jQuery('#datepicker').css("display", "none");
+        jQuery('#settings').css("display", "none");
     }
 }
 
 function calendar_handler() {
     if (jQuery('#datepicker').css("display") == "none") {
-    jQuery('#datepicker').toggle()
-    jQuery('#homepage').css("display", "none");
-    jQuery('#leaderboard').css("display", "none");
-    jQuery('#activity_feed').css("display", "none");
-    jQuery('#settings').css("display", "none");
+        jQuery('#datepicker').toggle()
+        jQuery('#homepage').css("display", "none");
+        jQuery('#leaderboard').css("display", "none");
+        jQuery('#activity_feed').css("display", "none");
+        jQuery('#settings').css("display", "none");
     }
 }
 
 function settings_handler() {
     if (jQuery('#settings').css("display") == "none") {
-    jQuery('#settings').toggle()
-    jQuery('#homepage').css("display", "none");
-    jQuery('#leaderboard').css("display", "none");
-    jQuery('#activity_feed').css("display", "none");
-    jQuery('#datepicker').css("display", "none");
+        jQuery('#settings').toggle()
+        jQuery('#homepage').css("display", "none");
+        jQuery('#leaderboard').css("display", "none");
+        jQuery('#activity_feed').css("display", "none");
+        jQuery('#datepicker').css("display", "none");
     }
 }
-
 function setup() {
     jQuery('#info').click(info_handler);
-    jQuery('#homepage_button').click(homepage_handler);
-    jQuery('#leaderboard_button').click(leaderboard_handler);
-    jQuery('#activity_button').click(activity_handler);
-    jQuery('#calendar_button').click(calendar_handler);
-    jQuery('#settings_button').click(settings_handler);
     $('#login').click(redirect_to_login);
     $('#signup').click(redirect_to_login);
 
