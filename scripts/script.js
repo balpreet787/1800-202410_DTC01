073cae6cf11012f4908490611e02301acdaa4fb7
Balpreet Sekhon <balpreetsekhon787@gmail.com>
2024-03-06 11:41:55 -0800

    connect login page to firebase

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,16 @@ function info_handler() {
 
     jQuery('#info-box').slideToggle()
 }
+function redirect_to_login() {
+    window.location.href = 'login.html';
+}
 
 
 function setup() {
     jQuery('#info').click(info_handler);
+    $('#login').click(redirect_to_login);
+    $('#signup').click(redirect_to_login);
+
     $("#datepicker").datepicker();
 }
 
