157964f4fe475c0db0881501d42ff0a1b70de435
Claudia <cle54@my.bcit.ca>
2024-03-06 16:23:26 -0800

    Begin connecting homepage to firebase

--- scripts/script.js
+++ scripts/script.js
@@ -15,9 +15,28 @@ function logout() {
     firebase.auth().signOut().then(() => {
         // Sign-out successful.
         console.log("logging out user");
-      }).catch((error) => {
+    }).catch((error) => {
         // An error happened.
-      });
+    });
+}
+
+function insertNameFromFirestore() {
+    // Check if the user is logged in:
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            console.log(user.uid); // Let's know who the logged-in user is by logging their UID
+            currentUser = db.collection("users").doc(user.uid); // Go to the Firestore document of the user
+            currentUser.get().then(userDoc => {
+                // Get the user name
+                let userName = userDoc.data().name;
+                console.log(userName);
+                //$("#name-goes-here").text(userName); // jQuery
+                document.getElementById("username").innerText = userName;
+            })
+        } else {
+            console.log("No user is logged in."); // Log a message when no user is logged in
+        }
+    })
 }
 
 function homepage_handler() {
@@ -143,4 +162,5 @@ function setup() {
     $("#datepicker").datepicker();
 }
 
+insertNameFromFirestore();
 jQuery(document).ready(setup);
