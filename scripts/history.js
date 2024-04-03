c0faeab6a30d7b70c2bd457c68892b1f9a5e18e1
Claudia <cle54@my.bcit.ca>
2024-04-02 17:46:41 -0700

    Merge branch

--- scripts/history.js
+++ scripts/history.js
@@ -1,4 +1,5 @@
 function showRecordedWorkouts(currentUser) {
+    console.log(currentUser)
     $("#recorded_workouts").empty();
     let inputDate = $('#selectedDate').val();
 
@@ -20,10 +21,7 @@ function showRecordedWorkouts(currentUser) {
                             <span><b>Workout:</b> ${workouts.data().exerciseType}</span>
                             <span><b>Intensity:</b> ${workouts.data().intensity}</span>
                             </div>
-                            <div>
-                            <button id="${workouts.id}Edit" class="p-2 self-start text-md">&#9998;</button>
                             <button id="${workouts.id}Delete" class="p-2 self-start text-md">X</button>
-                            </div>
                             </div>
                             <div class="flex flex-col justify-evenly bg-[#fff6e5] text-[16px] p-4">
                             <span><b>Calories burned:</b> ${workouts.data().calories}</span>
@@ -58,8 +56,6 @@ function showRecordedWorkouts(currentUser) {
                 );
             }
             deleteWorkoutHandler(currentUser, workouts.id);
-            jQuery(`#${workouts.id}Edit`).click(function(){updateworkout(currentUser, workouts.id)})
-            
         })
     });
 }
@@ -82,7 +78,7 @@ function calendarHandler() {
         jQuery('#add_workout').css("display", "none");
         jQuery('#filter_activity').css("display", "none");
         jQuery('#profile_info').css("display", "none");
-        
+
     }
 }
 
