25059f4161b7da405edc8223455041f6ba08f74f
Balpreet Sekhon <balpreetsekhon787@gmail.com>
2024-04-11 14:01:59 -0700

    Add activityfeed function headers

--- scripts/activityfeed.js
+++ scripts/activityfeed.js
@@ -1,3 +1,9 @@
+/**  Description: This file contains the functions that are used to display the activity feed on the activity feed page. 
+ * */
+
+/** Function to display the activity feed info on the activity feed page
+ * @param {Array} activityfeedinfo - an array of objects containing the user's friends' activity feed information
+ * */
 function displayActivityFeedInfo(activityfeedinfo) {
     const monthNames = [
         "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -6,30 +12,30 @@ function displayActivityFeedInfo(activityfeedinfo) {
     console.log(currentMonthName); // Output will be the current month name
 
     activityfeedinfo.sort((a, b) => b.startdate - a.startdate); // Sort the array of objects by date
-                let badgesEarned = null 
-                for (let i = 0; i < activityfeedinfo.length; i++) { 
-                    let startdate = activityfeedinfo[i]["startdate"].toDate(); 
-                    activityDate = startdate.getDate();
-                    activityMonth = monthNames[startdate.getMonth()];
-                    activityYear = startdate.getFullYear()
+    let badgesEarned = null
+    for (let i = 0; i < activityfeedinfo.length; i++) {
+        let startdate = activityfeedinfo[i]["startdate"].toDate();
+        activityDate = startdate.getDate();
+        activityMonth = monthNames[startdate.getMonth()];
+        activityYear = startdate.getFullYear()
 
-                    let activityDetails = ""
-                    let activityIntensity = ""
-                    if (activityfeedinfo[i]["exerciseType"] == "yoga") {
-                        activityfeedinfo[i]["exerciseType"] = "doing yoga";
-                    }
-                    if (activityfeedinfo[i]["exerciseType"] == "weightlifting" || activityfeedinfo[i]["exerciseType"] == "doing yoga") { // If the exercise type is weightlifting or yoga, display intensity
-                        activityDetails = ` spent ${activityfeedinfo[i]["workouttime"]} minutes <b>${activityfeedinfo[i]["exerciseType"]}</b>`
-                        activityIntensity = `<b>Intensity: </b> ${activityfeedinfo[i]["intensity"]}`
-                    }
-                    else { // If the exercise type is running or cycling, display distance
-                        activityDetails = ` spent ${activityfeedinfo[i]["workouttime"]} minutes <b>${activityfeedinfo[i]["exerciseType"]}</b>`
-                        activityIntensity = `<b>Distance: </b>${activityfeedinfo[i]["intensity"]} km`
+        let activityDetails = ""
+        let activityIntensity = ""
+        if (activityfeedinfo[i]["exerciseType"] == "yoga") {
+            activityfeedinfo[i]["exerciseType"] = "doing yoga";
+        }
+        if (activityfeedinfo[i]["exerciseType"] == "weightlifting" || activityfeedinfo[i]["exerciseType"] == "doing yoga") { // If the exercise type is weightlifting or yoga, display intensity
+            activityDetails = ` spent ${activityfeedinfo[i]["workouttime"]} minutes <b>${activityfeedinfo[i]["exerciseType"]}</b>`
+            activityIntensity = `<b>Intensity: </b> ${activityfeedinfo[i]["intensity"]}`
+        }
+        else { // If the exercise type is running or cycling, display distance
+            activityDetails = ` spent ${activityfeedinfo[i]["workouttime"]} minutes <b>${activityfeedinfo[i]["exerciseType"]}</b>`
+            activityIntensity = `<b>Distance: </b>${activityfeedinfo[i]["intensity"]} km`
 
-                    }
-                    if (activityfeedinfo[i]["badgesearned"] == badgesEarned || activityfeedinfo[i]["badgesearned"] == null) { // If the user has not earned a badge, display normal activity 
-                        activityfeedinfo[i]["badgesearned"] = "";
-                        addToActivityFeed = `<div class="flex flex-row bg-[#fff6e5] rounded-xl shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px] mt-2 mx-4 normal-activity activity-feed-post${i} ${activityfeedinfo[i]["nickname"].toLowerCase()}">
+        }
+        if (activityfeedinfo[i]["badgesearned"] == badgesEarned || activityfeedinfo[i]["badgesearned"] == null) { // If the user has not earned a badge, display normal activity 
+            activityfeedinfo[i]["badgesearned"] = "";
+            addToActivityFeed = `<div class="flex flex-row bg-[#fff6e5] rounded-xl shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px] mt-2 mx-4 normal-activity activity-feed-post${i} ${activityfeedinfo[i]["nickname"].toLowerCase()}">
                                             <img class="h-20 mx-5 self-center rounded-full w-20" src="${activityfeedinfo[i]["profilepic"]}" alt="">
                                             <div class="p-2 ">
                                                 <div class="py-2 flex">
@@ -38,10 +44,10 @@ function displayActivityFeedInfo(activityfeedinfo) {
                                                 <p class="text-xs pb-4 pr-1" id="activity-feed-phrase">${activityfeedinfo[i]["username"]} ${activityDetails}!</br><b>Calories Burned:</b> ${activityfeedinfo[i]["calories"]}</br>${activityIntensity}</p>
                                             </div>
                                         </div>`
-                    } else { // If the user has earned a badge, display accomplishment activity 
-                        badgesEarned = activityfeedinfo[i]["badgesearned"]
+        } else { // If the user has earned a badge, display accomplishment activity 
+            badgesEarned = activityfeedinfo[i]["badgesearned"]
 
-                        addToActivityFeed = `<div class="flex flex-row mx-4 ">
+            addToActivityFeed = `<div class="flex flex-row mx-4 ">
                                         </div>
                                         <div class="flex flex-row bg-[#fff6e5] rounded-xl shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px] mt-2 mx-4 accomplishment-activity activity-feed-post${i} ${activityfeedinfo[i]["nickname"]}">
                                         <img class="h-20 mx-5 self-center rounded-full w-20" src="${activityfeedinfo[i]["profilepic"]}" alt="">
@@ -53,12 +59,15 @@ function displayActivityFeedInfo(activityfeedinfo) {
                                                 <p class="text-xs pb-4 pr-1 ml-auto"  id="accomplishment-phrase">${activityfeedinfo[i]["username"]} ${activityDetails} and earned a ${activityfeedinfo[i]["badgeName"]}!</br><b>Calories Burned:</b> ${activityfeedinfo[i]["calories"]} </br> ${activityIntensity}</p>
                                             </div>
                                         </div>`
-                    }
-                    jQuery("#activity_feed_info").append(addToActivityFeed);
-                }
+        }
+        jQuery("#activity_feed_info").append(addToActivityFeed);
+    }
 
 }
 
+/** Function to get the user's friends' activity feed information
+ * @param {firebase.firestore.DocumentReference} currentUser - the current user object
+ * */
 function getActivityFeedInfo(currentUser) {
     jQuery("#activity_feed_info").empty(); // Clear the activity feed info div before adding new content
     var badgesEarned = null;
@@ -102,6 +111,9 @@ function getActivityFeedInfo(currentUser) {
 }
 
 
+/** Function to display the specific user's activity feed information
+ * @param {firebase.firestore.DocumentReference} currentUser - the current user object
+ * */
 function userSearchInActivityFeed() {
     var userSearched = $("input[name='username-search']").val().toLowerCase();
     if (userSearched == "") {
@@ -119,8 +131,8 @@ function userSearchInActivityFeed() {
     }
 }
 
-
-
+/** Function to filter the activity feed based on the user's selection of activity or accomplishment
+ * */
 function filterActivityFeed() {
     var selectedValue = $("input[name='filter-activity-feed']:checked").val();
     console.log(selectedValue)
@@ -136,10 +148,12 @@ function filterActivityFeed() {
         jQuery(".reset_button, .reset_filtered_page, #filter-accomplishment, .accomplishment-activity, #filter_activity").css("display", "none")
         jQuery('#filter-and-search, .normal-activity, #activity_feed').css('display', 'flex')
         jQuery('#activity_feed').css("flex-direction", "column");
-        
+
     }
 }
 
+/** Function to reset the filtered activity feed
+ * */
 function resetFilteredActivityFeed() {
     $("input[name='filter-activity-feed']").prop('checked', false);
     jQuery("input[name='username-search']").val("")
@@ -148,6 +162,8 @@ function resetFilteredActivityFeed() {
     jQuery('#activity_feed').css("flex-direction", "column");
 }
 
+/** Function to display the activity feed section and hide the other sections
+ * */
 function activityHandler() {
     if (jQuery('#activity_feed').css("display") == "none") {
         jQuery("#homepage-icon").attr('src', './images/nav-icons/home-black.svg')
@@ -162,6 +178,8 @@ function activityHandler() {
     }
 }
 
+/** Function to display the filter section and hide the other sections
+ * */
 function filterHandler() {
     if (jQuery('#filter_activity').css("display") == "none") {
         jQuery('#filter_activity').toggle();
